78e98eb43755d2211575611df2ad2a9c972338df
GarenLiang <ideabreeze@gmail.com>
2017-04-23 19:42:16 -0700

    Auth Flow :heart:

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, AsyncStorage } from 'react-native';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class AuthScreen extends Component {
   componentDidMount() {
     this.props.facebookLogin();
+    this.onAuthComplete(this.props);
+  }
+  componentWillReceiveProps(nextProps) {
+    this.onAuthComplete(nextProps);
+  }
+  onAuthComplete(props) {
+    if (props.token) {
+      this.props.navigation.navigate();
+    }
   }
   render() {
     return (
-      <View>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-        <Text>AuthScreen</Text>
-      </View>
+      <View />
     );
   }
 }
+function mapStateToProps({ auth }) {
+  return { token: auth.token };
+}
 
-export default connect(null, actions)(AuthScreen);
+export default connect(mapStateToProps, actions)(AuthScreen);
