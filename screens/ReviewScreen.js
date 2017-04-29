9947722cb0eae43305c69bfa3df8b8718c1c53ca
GarenLiang <ideabreeze@gmail.com>
2017-04-28 20:06:38 -0700

    Review Screen:four_leaf_clover:

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, Platform } from 'react-native';
-import { Button } from 'react-native-elements';
+import { View, Text, Platform, ScrollView, Linking} from 'react-native';
+import { Button, Card } from 'react-native-elements';
+import { connect } from 'react-redux';
 
 class ReviewScreen extends Component {
   static navigationOptions = {
@@ -21,18 +22,45 @@ class ReviewScreen extends Component {
       };
     }
   }
+  renderLikedJobs() {
+    return this.props.likedJobs.map(job => {
+      const { company, formattedRelativeTime, url } = job;
+      return (
+        <Card>
+          <View style={{ height: 200 }}>
+            <View style={styles.detailWrapper}>
+              <Text style={styles.italics}>{company}</Text>
+              <Text style={styles.italics}>{formattedRelativeTime}</Text>
+            </View>
+            <Button
+             title="Apply Now"
+             backgroundColor="#03A9F4"
+             onPress={() => Linking.openURL(url)}
+            />
+          </View>
+        </Card>
+      );
+    });
+  }
   render() {
     return (
-      <View>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-        <Text>ReviewScreen</Text>
-      </View>
+      <ScrollView>
+        {this.renderLikedJobs()}
+      </ScrollView>
     );
   }
 }
-
-export default ReviewScreen;
+const styles = {
+  italics: {
+    fontStyle: 'italic'
+  },
+  detailWrapper: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  }
+}
+function mapStateToProps(state) {
+  return { likedJobs: state.likedJobs };
+}
+export default connect(mapStateToProps)(ReviewScreen);
